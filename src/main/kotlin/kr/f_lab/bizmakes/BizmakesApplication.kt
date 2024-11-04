886e65fea50134d58d81d516a35d9db57c865ecd
김진홍 <deepred.kim@gmail.com>
2024-11-04 17:59:11 +0900

    chore: 코틀린으로 이전

---
+++ src/main/kotlin/kr/f_lab/bizmakes/BizmakesApplication.kt
@@ -0,0 +1,11 @@
+package kr.f_lab.bizmakes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BizmakesApplication
+
+fun main(args: Array<String>) {
+	runApplication<BizmakesApplication>(*args)
+}
